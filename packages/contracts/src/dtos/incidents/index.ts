531877670cd115ad27815ef0abe44bcfa30bb751
Carlo Aguirre <111251496+CarloAguirre@users.noreply.github.com>
2026-06-24 07:56:06 -0400

    feat(api): export incident 5c request contracts

--- packages/contracts/src/dtos/incidents/index.ts
+++ packages/contracts/src/dtos/incidents/index.ts
@@ -2,4 +2,6 @@ export * from './create-incident.request';
 export * from './update-incident.request';
 export * from './update-incident-status.request';
 export * from './create-incident-flash-report.request';
+export * from './create-incident-immediate-action.request';
+export * from './create-incident-investigation.request';
 export * from './incident.response';

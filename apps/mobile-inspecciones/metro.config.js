868662a256425b6718e4a9fa853f6e91738c7035
Carlo Aguirre <111251496+CarloAguirre@users.noreply.github.com>
2026-06-24 16:53:13 -0400

    fix(mobile): avoid watching root node_modules in metro

--- apps/mobile-inspecciones/metro.config.js
+++ apps/mobile-inspecciones/metro.config.js
@@ -3,10 +3,11 @@ const path = require('path');
 
 const projectRoot = __dirname;
 const monorepoRoot = path.resolve(projectRoot, '../..');
+const contractsRoot = path.resolve(monorepoRoot, 'packages/contracts');
 
 const config = getDefaultConfig(projectRoot);
 
-config.watchFolders = [monorepoRoot];
+config.watchFolders = [contractsRoot];
 config.resolver.nodeModulesPaths = [
   path.resolve(projectRoot, 'node_modules'),
   path.resolve(monorepoRoot, 'node_modules'),

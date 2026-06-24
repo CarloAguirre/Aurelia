35902a036dc2a55ef491e13923c24a42dcdc0c97
Carlo Aguirre <111251496+CarloAguirre@users.noreply.github.com>
2026-06-23 21:24:59 -0400

    feat(api): register inspection repositories

--- apps/api/src/modules/inspections/inspections.module.ts
+++ apps/api/src/modules/inspections/inspections.module.ts
@@ -1,11 +1,27 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { InspectionFormItemEntity } from './entities/inspection-form-item.entity';
+import { InspectionFormSectionEntity } from './entities/inspection-form-section.entity';
+import { InspectionFormTemplateEntity } from './entities/inspection-form-template.entity';
+import { InspectionItemResponseEntity } from './entities/inspection-item-response.entity';
+import { InspectionStateEntity } from './entities/inspection-state.entity';
+import { InspectionTypeEntity } from './entities/inspection-type.entity';
 import { InspectionEntity } from './entities/inspection.entity';
 import { InspectionsController } from './inspections.controller';
 import { InspectionsService } from './inspections.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([InspectionEntity])],
+  imports: [
+    TypeOrmModule.forFeature([
+      InspectionTypeEntity,
+      InspectionFormTemplateEntity,
+      InspectionFormSectionEntity,
+      InspectionFormItemEntity,
+      InspectionEntity,
+      InspectionItemResponseEntity,
+      InspectionStateEntity,
+    ]),
+  ],
   controllers: [InspectionsController],
   providers: [InspectionsService],
   exports: [InspectionsService],

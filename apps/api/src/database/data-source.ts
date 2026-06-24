3cc8b295746e1f8f2defd3478d580ae07e787f1c
Carlo Aguirre <111251496+CarloAguirre@users.noreply.github.com>
2026-06-23 21:26:23 -0400

    feat(api): register inspection entities in datasource

--- apps/api/src/database/data-source.ts
+++ apps/api/src/database/data-source.ts
@@ -1,6 +1,21 @@
 import 'reflect-metadata';
 import { config } from 'dotenv';
 import { DataSource } from 'typeorm';
+import { AuditLogEntity } from '../modules/audit/entities/audit-log.entity';
+import { CommentEntity } from '../modules/comments/entities/comment.entity';
+import { EntityReferenceTypeEntity } from '../modules/evidences/entities/entity-reference-type.entity';
+import { EvidenceLinkEntity } from '../modules/evidences/entities/evidence-link.entity';
+import { EvidenceEntity } from '../modules/evidences/entities/evidence.entity';
+import { FileEntity } from '../modules/files/entities/file.entity';
+import { InspectionFindingEntity } from '../modules/inspections/entities/inspection-finding.entity';
+import { InspectionFollowupEntity } from '../modules/inspections/entities/inspection-followup.entity';
+import { InspectionFormItemEntity } from '../modules/inspections/entities/inspection-form-item.entity';
+import { InspectionFormSectionEntity } from '../modules/inspections/entities/inspection-form-section.entity';
+import { InspectionFormTemplateEntity } from '../modules/inspections/entities/inspection-form-template.entity';
+import { InspectionItemResponseEntity } from '../modules/inspections/entities/inspection-item-response.entity';
+import { InspectionStateEntity } from '../modules/inspections/entities/inspection-state.entity';
+import { InspectionTypeEntity } from '../modules/inspections/entities/inspection-type.entity';
+import { InspectionEntity } from '../modules/inspections/entities/inspection.entity';
 import { AreaEntity } from '../modules/organization/entities/area.entity';
 import { BusinessUnitEntity } from '../modules/organization/entities/business-unit.entity';
 import { CompanyEntity } from '../modules/organization/entities/company.entity';
@@ -14,16 +29,10 @@ import { UserAreaEntity } from '../modules/users/entities/user-area.entity';
 import { UserCompanyEntity } from '../modules/users/entities/user-company.entity';
 import { UserRoleEntity } from '../modules/users/entities/user-role.entity';
 import { UserEntity } from '../modules/users/entities/user.entity';
-import { FileEntity } from '../modules/files/entities/file.entity';
-import { EntityReferenceTypeEntity } from '../modules/evidences/entities/entity-reference-type.entity';
-import { EvidenceEntity } from '../modules/evidences/entities/evidence.entity';
-import { EvidenceLinkEntity } from '../modules/evidences/entities/evidence-link.entity';
-import { CommentEntity } from '../modules/comments/entities/comment.entity';
-import { AuditLogEntity } from '../modules/audit/entities/audit-log.entity';
-import { WorkflowDefinitionEntity } from '../modules/workflows/entities/workflow-definition.entity';
 import { WorkflowDefinitionStepEntity } from '../modules/workflows/entities/workflow-definition-step.entity';
-import { WorkflowInstanceEntity } from '../modules/workflows/entities/workflow-instance.entity';
+import { WorkflowDefinitionEntity } from '../modules/workflows/entities/workflow-definition.entity';
 import { WorkflowInstanceStepEntity } from '../modules/workflows/entities/workflow-instance-step.entity';
+import { WorkflowInstanceEntity } from '../modules/workflows/entities/workflow-instance.entity';
 
 config();
 
@@ -62,6 +71,15 @@ export const AppDataSource = new DataSource({
     WorkflowDefinitionStepEntity,
     WorkflowInstanceEntity,
     WorkflowInstanceStepEntity,
+    InspectionTypeEntity,
+    InspectionFormTemplateEntity,
+    InspectionFormSectionEntity,
+    InspectionFormItemEntity,
+    InspectionEntity,
+    InspectionItemResponseEntity,
+    InspectionFindingEntity,
+    InspectionFollowupEntity,
+    InspectionStateEntity,
   ],
   migrations: ['src/database/migrations/*.ts'],
   synchronize: false,

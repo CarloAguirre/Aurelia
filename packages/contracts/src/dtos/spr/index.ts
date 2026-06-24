a576dac17a38aebbffd2fbeb5a9d23d274515278
Carlo Aguirre <111251496+CarloAguirre@users.noreply.github.com>
2026-06-24 10:20:13 -0400

    feat(spr): export SPR dto contracts

---
+++ packages/contracts/src/dtos/spr/index.ts
@@ -0,0 +1,4 @@
+export * from './create-spr-monthly-record.request';
+export * from './update-spr-monthly-record.request';
+export * from './approve-spr-monthly-record.request';
+export * from './spr.response';

d11061dab3e090419eb9b10621ef2ecdc7f09c0f
Carlo Aguirre <111251496+CarloAguirre@users.noreply.github.com>
2026-06-23 21:24:13 -0400

    feat(api): export phase 4b inspection contracts

--- packages/contracts/src/dtos/inspections/index.ts
+++ packages/contracts/src/dtos/inspections/index.ts
@@ -1,3 +1,5 @@
 export * from './create-inspection.request';
+export * from './update-inspection.request';
 export * from './update-inspection-status.request';
+export * from './upsert-inspection-answer.request';
 export * from './inspection.response';

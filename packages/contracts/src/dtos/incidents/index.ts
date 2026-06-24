3b830e4d776a8bc00c77484c3ef0d52e53d2ad9f
Carlo Aguirre <111251496+CarloAguirre@users.noreply.github.com>
2026-06-24 07:44:42 -0400

    feat(contracts): export incident phase 5b dtos

--- packages/contracts/src/dtos/incidents/index.ts
+++ packages/contracts/src/dtos/incidents/index.ts
@@ -1,2 +1,5 @@
 export * from './create-incident.request';
+export * from './update-incident.request';
+export * from './update-incident-status.request';
+export * from './create-incident-flash-report.request';
 export * from './incident.response';

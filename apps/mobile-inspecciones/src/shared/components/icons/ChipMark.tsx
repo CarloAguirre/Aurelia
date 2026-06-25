99c5be042e018e28c228dd63eb490b728e6819b5
Carlo Aguirre <111251496+CarloAguirre@users.noreply.github.com>
2026-06-25 06:54:11 -0400

    add chip icon

---
+++ apps/mobile-inspecciones/src/shared/components/icons/ChipMark.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Svg, { Path } from 'react-native-svg';
+
+export function ChipMark() {
+  return (
+    <Svg width={28} height={22} viewBox="0 0 28 22">
+      <Path d="M8 3h12v16H8z" fill="#fff" />
+    </Svg>
+  );
+}

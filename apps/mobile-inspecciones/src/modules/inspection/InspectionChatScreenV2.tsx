834ecea6c5c0b814bf98d885e05cf78bac94d23a
Carlo Aguirre <111251496+CarloAguirre@users.noreply.github.com>
2026-06-27 11:59:41 -0400

    chore(mobile): test enum content

--- apps/mobile-inspecciones/src/modules/inspection/InspectionChatScreenV2.tsx
+++ apps/mobile-inspecciones/src/modules/inspection/InspectionChatScreenV2.tsx
@@ -1,1 +1,6 @@
+import { InspectionAnswerValue } from '@aurelia/contracts';
+
+const value = InspectionAnswerValue.NOT_COMPLIANT;
+
 export { InspectionAssistantChatScreen as InspectionChatScreenV2 } from './InspectionAssistantChatScreen';
+export { value };

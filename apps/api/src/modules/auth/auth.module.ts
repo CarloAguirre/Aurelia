8570cc278fa5abd240e9ff705d71169afa17e5ed
Carlo Aguirre <111251496+CarloAguirre@users.noreply.github.com>
2026-06-29 10:56:13 -0400

    feat(api): register user sessions repository

--- apps/api/src/modules/auth/auth.module.ts
+++ apps/api/src/modules/auth/auth.module.ts
@@ -5,12 +5,13 @@ import { UserEntity } from '../users/entities/user.entity';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { CredentialHashService } from './credential-hash.service';
+import { UserSessionEntity } from './entities/user-session.entity';
 import { JwtAuthGuard } from './jwt-auth.guard';
 import { JwtTokenService } from './jwt-token.service';
 import { PermissionsGuard } from './permissions.guard';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
+  imports: [TypeOrmModule.forFeature([UserEntity, UserSessionEntity])],
   controllers: [AuthController],
   providers: [
     AuthService,

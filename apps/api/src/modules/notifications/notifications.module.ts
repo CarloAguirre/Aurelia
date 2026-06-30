42f5fd545b3451ee68a862adc3ab6d29c278797c
Carlo Aguirre <111251496+CarloAguirre@users.noreply.github.com>
2026-06-30 07:58:56 -0400

    feat(api): configure notifications module

--- apps/api/src/modules/notifications/notifications.module.ts
+++ apps/api/src/modules/notifications/notifications.module.ts
@@ -1,4 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { NotificationsController } from './notifications.controller';
+import { NotificationsService } from './notifications.service';
+import { NotificationEntity } from './entities/notification.entity';
+import { NotificationRecipientEntity } from './entities/notification-recipient.entity';
 
-@Module({})
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      NotificationEntity,
+      NotificationRecipientEntity,
+    ]),
+  ],
+  controllers: [NotificationsController],
+  providers: [NotificationsService],
+  exports: [NotificationsService],
+})
 export class NotificationsModule {}

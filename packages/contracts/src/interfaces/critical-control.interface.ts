9442d2e988336179797e117739bbedaf47729958
Carlo Aguirre <111251496+CarloAguirre@users.noreply.github.com>
2026-06-30 11:50:28 -0400

    chore(contracts): remove obsolete critical control interface

--- packages/contracts/src/interfaces/critical-control.interface.ts
+++
@@ -1,9 +0,0 @@
-import type { ID } from '../types/common';
-import type { BaseEntity } from './entity.interface';
-
-export interface CriticalControl extends BaseEntity {
-  name: string;
-  code: string;
-  description: string | null;
-  areaId: ID;
-}

674f30582df21f397f3fef019828c7917708e5ab
Carlo Aguirre <111251496+CarloAguirre@users.noreply.github.com>
2026-06-30 11:51:31 -0400

    feat(contracts): update mue responses

--- packages/contracts/src/dtos/mue/mue.response.ts
+++ packages/contracts/src/dtos/mue/mue.response.ts
@@ -1,3 +1,7 @@
-import type { Mue } from '../../interfaces/mue.interface';
+import type { ControlAreaAssignment, ControlVerificationItem, CriticalControl, Mue, MueDetail } from '../../interfaces/mue.interface';
 
 export type MueResponse = Mue;
+export type MueDetailResponse = MueDetail;
+export type CriticalControlResponse = CriticalControl;
+export type ControlVerificationItemResponse = ControlVerificationItem;
+export type ControlAreaAssignmentResponse = ControlAreaAssignment;

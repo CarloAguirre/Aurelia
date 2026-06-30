42ade019bc0e2cf688a2016cfe2d1b402094d4b2
Carlo Aguirre <111251496+CarloAguirre@users.noreply.github.com>
2026-06-30 07:59:58 -0400

    feat(api): import notifications module in workflows

--- apps/api/src/modules/workflows/workflows.module.ts
+++ apps/api/src/modules/workflows/workflows.module.ts
@@ -1,15 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { WorkflowDefinitionEntity } from './entities/workflow-definition.entity';
-import { WorkflowDefinitionStepEntity } from './entities/workflow-definition-step.entity';
-import { WorkflowInstanceEntity } from './entities/workflow-instance.entity';
-import { WorkflowInstanceStepEntity } from './entities/workflow-instance-step.entity';
 import { EntityReferenceTypeEntity } from '../evidences/entities/entity-reference-type.entity';
+import { NotificationsModule } from '../notifications/notifications.module';
+import { WorkflowDefinitionStepEntity } from './entities/workflow-definition-step.entity';
+import { WorkflowDefinitionEntity } from './entities/workflow-definition.entity';
+import { WorkflowInstanceStepEntity } from './entities/workflow-instance-step.entity';
+import { WorkflowInstanceEntity } from './entities/workflow-instance.entity';
 import { WorkflowsController } from './workflows.controller';
 import { WorkflowsService } from './workflows.service';
 
 @Module({
   imports: [
+    NotificationsModule,
     TypeOrmModule.forFeature([
       WorkflowDefinitionEntity,
       WorkflowDefinitionStepEntity,

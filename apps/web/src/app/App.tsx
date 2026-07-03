8da8ec226e48f70cf2290de2504b9a4a3337e218
Carlo Aguirre <111251496+CarloAguirre@users.noreply.github.com>
2026-07-03 13:20:13 -0400

    Mount new inspection iframe modal controller

--- apps/web/src/app/App.tsx
+++ apps/web/src/app/App.tsx
@@ -1,9 +1,13 @@
 import { Outlet } from 'react-router-dom';
+import { NewInspectionIframeModalController } from '../modules/inspections/components/NewInspectionIframeModalController';
 
 const RouterOutlet = Outlet as unknown as () => JSX.Element | null;
 
 export function App() {
   return (
-    <RouterOutlet />
+    <>
+      <RouterOutlet />
+      <NewInspectionIframeModalController />
+    </>
   );
 }

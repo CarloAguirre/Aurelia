0f80c6893dacbf95741020a8791d83d9051cd947
Carlo Aguirre <111251496+CarloAguirre@users.noreply.github.com>
2026-07-15 13:41:39 -0400

    Registra servicios PDF comunes

--- apps/api/src/modules/reports/reports.module.ts
+++ apps/api/src/modules/reports/reports.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { FilesModule } from '../files/files.module';
 import { InspectionFindingEntity } from '../inspections/entities/inspection-finding.entity';
 import { InspectionTypeEntity } from '../inspections/entities/inspection-type.entity';
 import { InspectionEntity } from '../inspections/entities/inspection.entity';
@@ -7,8 +8,10 @@ import { AreaEntity } from '../organization/entities/area.entity';
 import { CompanyEntity } from '../organization/entities/company.entity';
 import { SectorEntity } from '../organization/entities/sector.entity';
 import { UserEntity } from '../users/entities/user.entity';
+import { InspectionDetailReportPdfService } from './inspection-detail-report-pdf.service';
 import { InspectionPeriodicReportController } from './inspection-periodic-report.controller';
 import { InspectionPeriodicReportService } from './inspection-periodic-report.service';
+import { ReportPdfService } from './report-pdf.service';
 import { ReportPeriodService } from './report-period.service';
 import { ReportScopeService } from './report-scope.service';
 import { ReportsController } from './reports.controller';
@@ -16,6 +19,7 @@ import { ReportsService } from './reports.service';
 
 @Module({
   imports: [
+    FilesModule,
     TypeOrmModule.forFeature([
       InspectionEntity,
       InspectionFindingEntity,
@@ -27,7 +31,14 @@ import { ReportsService } from './reports.service';
     ]),
   ],
   controllers: [ReportsController, InspectionPeriodicReportController],
-  providers: [ReportsService, ReportPeriodService, ReportScopeService, InspectionPeriodicReportService],
-  exports: [ReportPeriodService, ReportScopeService],
+  providers: [
+    ReportsService,
+    ReportPdfService,
+    ReportPeriodService,
+    ReportScopeService,
+    InspectionPeriodicReportService,
+    InspectionDetailReportPdfService,
+  ],
+  exports: [ReportPdfService, ReportPeriodService, ReportScopeService, InspectionDetailReportPdfService],
 })
 export class ReportsModule {}

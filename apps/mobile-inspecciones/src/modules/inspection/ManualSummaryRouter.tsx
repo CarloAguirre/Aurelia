d0a421f1c627c3720844dbaefda207b48e68d059
Carlo Aguirre <111251496+CarloAguirre@users.noreply.github.com>
2026-07-21 18:03:57 -0400

    Usa enum para resolver resumen mobile

--- apps/mobile-inspecciones/src/modules/inspection/ManualSummaryRouter.tsx
+++ apps/mobile-inspecciones/src/modules/inspection/ManualSummaryRouter.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { InspectionType } from '@aurelia/contracts';
 import { useManualInspectionDraft } from './manualInspection.store';
 import { ManualFindingSummaryScreen } from './ManualFindingSummaryScreen';
 import { ManualInspectionSummaryFixedScreen } from './ManualInspectionSummaryFixedScreen';
 
 export function ManualSummaryRouter() {
-  const inspectionTypeLabel = useManualInspectionDraft((state) => state.inspectionTypeLabel);
-  if (inspectionTypeLabel === 'Hallazgo') return <ManualFindingSummaryScreen />;
+  const inspectionType = useManualInspectionDraft((state) => state.inspectionType);
+  if (inspectionType === InspectionType.ENVIRONMENTAL) return <ManualFindingSummaryScreen />;
   return <ManualInspectionSummaryFixedScreen />;
 }

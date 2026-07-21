4c9b1808fbb577b8ceea891b5b575b8a9cbd8064
Carlo Aguirre <111251496+CarloAguirre@users.noreply.github.com>
2026-07-21 18:07:17 -0400

    Protege resumen sin tipo seleccionado

--- apps/mobile-inspecciones/src/modules/inspection/ManualSummaryRouter.tsx
+++ apps/mobile-inspecciones/src/modules/inspection/ManualSummaryRouter.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Redirect } from 'expo-router';
 import { InspectionType } from '@aurelia/contracts';
 import { useManualInspectionDraft } from './manualInspection.store';
 import { ManualFindingSummaryScreen } from './ManualFindingSummaryScreen';
@@ -6,6 +7,9 @@ import { ManualInspectionSummaryFixedScreen } from './ManualInspectionSummaryFix
 
 export function ManualSummaryRouter() {
   const inspectionType = useManualInspectionDraft((state) => state.inspectionType);
+  const inspectionTypeSelected = useManualInspectionDraft((state) => state.inspectionTypeSelected);
+
+  if (!inspectionTypeSelected) return <Redirect href="/inspection/manual/type" />;
   if (inspectionType === InspectionType.ENVIRONMENTAL) return <ManualFindingSummaryScreen />;
   return <ManualInspectionSummaryFixedScreen />;
 }
